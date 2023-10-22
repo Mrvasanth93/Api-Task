548c9e482d158c4634cc558a376061822db65771
Mr.Vasanth <145196660+Mrvasanth93@users.noreply.github.com>
2023-10-22 21:54:25 +0530

    Update Run.js

--- Run.js
+++ Run.js
@@ -23,7 +23,4 @@ function details(msg){
         }   
     }
 }
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
+
